5e6ccb2dbfd5a90abf40bfed86dc43fbe73a1253
Mshavri <eatyov@gmail.com>
2025-10-24 12:37:59 +0300

    تغطية الخلفية بمكعبات شطرنج

---
+++ Assets/Tile/MyTile.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Tile", menuName = "2D/Tiles/Custom Tile")]
+public class MyTile : Tile
+{
+    // تقدر تضيف خصائص هنا لاحقاً لو حبيت
+}
